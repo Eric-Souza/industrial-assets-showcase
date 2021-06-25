209b95e91dccf4ab54262c07e235dba722fc801a
Eric Souza <eric.bh18souza@gmail.com>
2021-06-25 13:50:30 -0300

    Api working successfully

---
+++ src/components/Body/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Body: React.FC = () => (
+  <Container>
+    Body
+  </Container>
+)
+
+export default Body
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 import { Container } from './styles'
 
+import Header from '../Header'
+import Body from '../Body'
+import Footer from '../Footer'
+
 const MainPage: React.FC = () => (
-  <Container>Página principal</Container>
+  <Container>
+    <Header />
+    <Body />
+    <Footer />
+  </Container>
 )
 
 export default MainPage
